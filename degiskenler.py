f9b778fa783f246ec8783f15df037a2128515f38
seydaazenler <seydaazenler@gmail.com>
2020-05-03 16:45:25 +0300

    Add Python project commit

---
+++ degiskenler.py
@@ -0,0 +1,14 @@
+isim = "Şeyda Zenler"   #değişken tanımlama
+sayi = 10
+tc = "12345678910"
+floatsayi = 10.5
+durum = False
+
+print(type(isim))
+print(type(sayi))
+print(type(tc))
+print(type(floatsayi))
+print(type(durum))
+
+
+
